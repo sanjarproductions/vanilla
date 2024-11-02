be76828f1cabb7a0315dd5d9171f601fe48279d4
Sanjar Kamaliddinov <sanjarkama26@gmail.com>
2024-11-02 11:54:53 +0500

    1 lesson complete fully inshallah

--- lesson-1/app.js
+++ lesson-1/app.js
@@ -4,14 +4,24 @@ var name = "Sanjar";
 
 // var => variable - global,older
 // let - new,local
+// const => contanta - will never change it's value after it's assigned and its not a varialbe 
 
 // Biggeste differense in real: "scope"
 
-if(true){
-   var a = 1;
-   let a = 1;
-}
-console.log(a)
+// if(true){
+//    var a = 1;
+//    let a = 1;
+// }
+// console.log(a)
+
+// Const example (how to not do)
+
+const k = 8;
+k = 20;
+console.log(k) // this is gonna give an error
 
 // Note: only in case of 5 + "5" JS will 55
 // but if you 5 * "5" it will give you 25 (and all other operations as well)
+
+
+// Data Types in JS = string, number, object, boolean, null, undefined, BingInt, Symbol
