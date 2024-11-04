75451696fc5820257179c667ba979e8f974cf4a0
Sanjar Kamaliddinov <sanjarkama26@gmail.com>
2024-11-04 21:38:31 +0500

    lesson 4 complete

---
+++ lesson-5/app.js
@@ -0,0 +1,38 @@
+// fucntion
+// solving some problems
+// parametr va argument
+// return 
+
+
+// let number = prompt("Enter a number")
+// let arr = []
+// let sum = 0;
+
+// for (i = 1; i <= number; i++) {
+//     sum += i // sum = sum + i 
+// }
+
+// for (i = 1; i <= number; i++) {
+//     if (i % 2 == 0) {
+//         sum += i // sum = sum + i 
+//     }
+// }
+
+// console.log(sum)
+
+
+// Functions are there for us to use a specific code in a specific way and time, otherwise if we write code outside of a function it will get invoked as soon as your browser read the JS file
+// const btn = document.querySelector("#btn");
+
+// btn.addEventListener("click", invokeFunction);
+
+// function invokeFunction() {
+//     document.body.style.backgroundColor = "red"
+// }
+
+
+// Parametr Va Argumentlar (teacher said we will need this ehile working with API's)
+
+function greet() {
+    return
+}
