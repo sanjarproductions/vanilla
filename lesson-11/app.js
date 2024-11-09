ea0a8692e1d5cfc7f6e4b0607fd61931ee00ac31
Sanjar Kamaliddinov <sanjarkama26@gmail.com>
2024-11-09 21:52:16 +0500

    .

---
+++ lesson-11/app.js
@@ -0,0 +1,31 @@
+// flat
+// let arr = [1, 2, 3, 4, 5, 6, 7, [1, 2, 3, 4, [1, 2, 3, 4, 5, [2, 23, 3, 4]]]];
+
+// let newVersion = arr.flat(999);
+// console.log(newVersion)
+
+// recursion
+// let num = 0;
+
+// function again() {
+//     num += 1;
+//     console.log(num)
+
+//     if (num < 10) {
+//         again()
+//     }
+
+// }
+// again()
+
+
+// object methods
+// let user = {
+//     name: "Sanjar",
+//     surename: "Kamaliddinov",
+//     age: 18
+// }
+
+// console.log(Object.keys(user))
+// console.log(Object.values(user))
+// console.log(Object.entries(user))
