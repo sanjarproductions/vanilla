3dd48ef08d6f2109f0c71b026e09add808e756eb
Sanjar Kamaliddinov <sanjarkama26@gmail.com>
2024-11-10 13:13:08 +0500

    lesson 12 complete

---
+++ lesson-12/app.js
@@ -0,0 +1,45 @@
+// Construstor function
+
+// function Student(){
+//     this.age = 18
+//     this.fistname = "Sanjar"
+// }
+
+// let object = new Student()
+// console.log(object)
+
+
+// PROTOTYPE
+// Array.prototype - for arrays
+// Object.prototype - for objects
+// Number.prototype - for numbers
+
+// String.prototype.reverseWord = function () {
+//     //  console.log(this)
+//     return this.split("").reverse().join("")
+// }
+
+// let word = "Sanjar".reverseWord()
+// console.log(word)
+
+// ________________________________________________________________________________________________________
+
+// Array.prototype.filterArray = function (max) {
+//     // console.log(this)
+//     return this.filter(n => n < max)
+// }
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 9].filterArray(10); // we can put any desireble value (like 10 in this case)
+// console.log(arr)
+
+// ________________________________________________________________________________________________________
+
+// IIFE Function
+
+(function () {
+    // …
+})();
+
+(() => {
+    // …
+})();
