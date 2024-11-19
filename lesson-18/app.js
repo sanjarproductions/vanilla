5ff03ddaf86075111ee0ef57e81aa12099eca608
Sanjar Kamaliddinov <sanjarkama26@gmail.com>
2024-11-19 12:41:36 +0500

    lesson 18 is alhamdulillah fully completed

--- lesson-18/app.js
+++ lesson-18/app.js
@@ -1,21 +1,42 @@
-// let head = document.querySelector("h1");
-
-// console.log(getComputedStyle(head).color) // try using you are gonn get it
-
-let users = [
-    {
-        username:"Sanjar",
-        
-    },
-    {
-        username:"Ryan",
-
-    },
-]
-
-for(let i in users){
-    console.log(i)
-}
-
 // for in
 // for of
+// table render
+// local storage practice !!!!!!
+// Math
+
+// let head = document.querySelector("h1");
+// console.log(getComputedStyle(head).color) // try using you are gonn get it
+
+// let table = document.querySelector("table")
+
+// let users = [
+//     {
+//         username: "Sanjar",
+
+//     },
+//     {
+//         username: "Ryan",
+
+//     },
+// ]
+
+// for (let i in users) {
+//     let th = document.createElement("th")
+//     th.innerHTML = users[i].username;
+//     table.appendChild(th)
+// }
+
+
+
+// Math
+// console.log(Math.min(1, 2, 3))
+// let numbers = [1, 2, 3]
+// console.log(Math.max(...numbers)) // have to parse it out
+
+Math.random() // gives random value
+Math.floor() // 5.9 => 5 
+Math.abs() // -9 => 9
+Math.ceil() // 5.1 => 6
+Math.round() // normal round in math
+Math.trunc() // no idea
+Math.sqrt() // корень
