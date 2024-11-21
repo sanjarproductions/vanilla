3278d5046e7348805e082e97fef7ef87eb61bd21
Sanjar Kamaliddinov <sanjarkama26@gmail.com>
2024-11-21 18:36:24 +0500

    lesson 21 is completed

---
+++ lesson-21/app.js
@@ -0,0 +1,49 @@
+
+
+
+// fetch("https://cat-fact.herokuapp.com/facts")
+//     .then((response) => response.json())
+//     // .then((result) => console.log(result))
+//     .then((result) => {
+//         result.map((i) => {
+//             const p = document.createElement("p");
+//             p.innerHTML = i.text
+//             document.body.appendChild(p)
+//         })
+//     })
+
+
+// async function fetching() {
+//     const response = await fetch("https://cat-fact.herokuapp.com/facts")
+//     const result = await response.json()
+//     // console.log(result)
+//     result.map((i) => {
+//         const p = document.createElement("p");
+//         p.innerHTML = i.text             
+//         return document.body.appendChild(p)
+//     })
+// }
+
+// fetching()
+
+//_______________________________
+// async function renderImg(name) {
+//     const response = await fetch(`https://ui-avatars.com/api/?name=${name}`)
+//     // const result = await response.json()
+//     // console.log(result)
+//     console.log(response)
+
+//     const img = document.createElement("img")
+//     img.src = response.url
+//     document.body.appendChild(img)
+// }
+
+// renderImg("Sanjar+Kamaliddinov")
+
+
+
+// event loop
+
+
+
+
