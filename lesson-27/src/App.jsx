0898edf8d901fb401df45ab3c13c5c765072725f
Sanjar Kamaliddinov <sanjarkama26@gmail.com>
2024-11-27 11:22:17 +0500

    lesson 27 - useState is alhamdulillah

--- lesson-27/src/App.jsx
+++ lesson-27/src/App.jsx
@@ -3,11 +3,13 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0) // topic 1
+  // const [count, setCount] = useState(0) // topic 1
+  // const [msg, setMsg] = useState();
+  const [name, setName] = useState("Sanjar")
 
-  function addOne() {
-    setCount(count + 1)
-  }
+  // function addOne() {
+  //   setCount(count + 1)
+  // }
 
   // let num = 0;
   // function vanilla() {
@@ -16,11 +18,34 @@ function App() {
   // }
   // if I'm gonna write console.log() her it's not gonna show the change 
 
+  // function takeValue(val) {
+  //   // console.log(smt)
+  //   setMsg(val)
+  // }
+
+  function changeName() {
+    if (name == "Sanjar") {
+      setName("Mike")
+    }
+    else {
+      setName("Sanjar")
+    }
+  }
   return (
     <>
-      <p>{count}</p>
-      <button onClick={addOne}>React JS</button>
+      <div className="wrapper" style={{ background: name == "Sanjar" ? "red" : "green" }}> {/*conditional styling*/}
+        <h2>{name}</h2>
+        <button onClick={changeName}>Click</button>
+      </div>
+      
+      {/* <p>{count}</p> */}
+      {/* <button onClick={addOne}>React JS</button> */}
+
       {/* <button onClick={vanilla}>Vanilla</button> */}
+
+      {/* <h2>{msg?.split(" ").length}</h2>
+      <textarea name="" id="" onChange={(e) => takeValue(e.target.value)} ></textarea> */}
+
     </>
   )
 }
