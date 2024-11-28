84bcb154eee646ee579215944380a3bf3f4bae5f
Sanjar Kamaliddinov <sanjarkama26@gmail.com>
2024-11-28 21:08:57 +0500

    lesson 28 is completed, alhamdulillah

---
+++ lesson-28/src/App.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useRef } from 'react'
+import './App.css'
+
+function App() {
+  // const [text, setText] = useState([])
+
+  // function submit(e){
+  //   e.preventDefault()
+  //   console.log(text)
+  // }
+
+  // ________
+  // useEffect(() => {
+  //   let isFetched = true;
+  //   fetch("https://jsonplaceholder.typicode.com/users")
+  //     .then(response => response.json())
+  //     .then(data => setText(data))
+
+  //   return () => {
+  //     isFetched = false // clean up function
+  //   }
+  // }, [])
+  // console.log(text)
+
+  // _________
+  const btn = useRef(null);
+
+  useEffect(() => {
+    btn.current.style.background = "red"
+  }, [])
+  
+  return (
+    <>
+      {/* <form onSubmit={submit}>
+        <input type="text" placeholder='Enter' onChange={(e) => setText(e.target.value)}/>
+        <button >Click</button>
+      </form> */}
+      <button ref={btn} className='button'>Click</button>
+    </>
+  )
+}
+
+export default App
+
+
+// conditional rendering => is normal rendering but with if statement
+// useEffect()
+// axios
+// useRef => aka query selector for react js, dont use .querySelector()
