94815461954941da9831308e32c17e9116547a69
Sanjar Kamaliddinov <sanjarkama26@gmail.com>
2024-12-01 14:26:10 +0500

    lesson 19 is complete

--- lesson-29/src/App.jsx
+++ lesson-29/src/App.jsx
@@ -4,11 +4,13 @@ import './App.css'
 import { Routes, Route } from "react-router-dom"
 import About from './pages/About'
 import Home from './pages/Home'
+import { NavLink } from 'react-router-dom'
+
+import Auth from './pages/Auth'
+import Login from './routes/Login'
+import Signup from './routes/Signup'
 
 function App() {
-
-
-
 
   // const [users, setUsers] = useState([])
   // useEffect(() => {
@@ -32,12 +34,18 @@ function App() {
   // console.log(users)
   return (
     <>
-      <nav>
-        <h1>Nav</h1>
+      <nav style={{background:"red"}}>
+        <NavLink className={({ isActive }) => isActive ? "active" : "link"} to="/">Home</NavLink> <br />
+        <NavLink className={({ isActive }) => isActive ? "active" : "link"} to="/about">About</NavLink> <br />
+        <NavLink className={({ isActive }) => isActive ? "active" : "link"} to="/auth">Auth</NavLink>
       </nav>
       <Routes>
+        <Route path='/' element={<Home />} /> 
         <Route path='/about' element={<About />} />
-        <Route path='/' element={<Home />} />
+        <Route path='/auth' element={<Auth />}>
+          <Route path='/auth/login' element={<Login />} />
+          <Route path='/auth/signup' element={<Signup />} />
+        </Route>
       </Routes>
     </>
   )
---
+++ lesson-29/src/pages/Auth.jsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+import { Outlet } from "react-router-dom"
+const Auth = () => {
+  return (
+    <div>
+      Auth page
+
+      <Outlet/>
+    </div>
+  )
+}
+
+export default Auth
